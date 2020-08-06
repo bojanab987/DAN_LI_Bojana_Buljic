e17a987894710db93bb803b09436b416483767e2
bojanabb <bojana.buljic@gmail.com>
2020-08-06 13:52:42 +0200

    Validation rules classes add

---
+++ DAN_LI_Bojana_Buljic/Zadatak_1/Validations/HealthInsuranceCardNoValidation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using Zadatak_1.Model;
+using Zadatak_1.Services;
+
+namespace Zadatak_1.Validations
+{
+    /// <summary>
+    /// Validation class for Health insurance cardno.
+    /// </summary>
+    class HealthInsuranceCardNoValidation : ValidationRule
+    {
+        /// <summary>
+        /// Method for health insurance card number input
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string number = value as string;
+            if (number.Length != 11 || !number.All(Char.IsDigit))
+            {
+                return new ValidationResult(false, "Number must contain 10 digits.");
+            }
+            else
+            {
+                Serivce ser = new Serivce();
+                List<tblPatient> patients = ser.GetAllPatients();
+                var list = patients.Where(x => x.HealthInsuranceCardNo == number).ToList();                
+                if (list.Count() > 0)
+                {
+                    return new ValidationResult(false, "This account number already exists.");
+                }
+                else
+                {
+                    return new ValidationResult(true, null);
+                }
+            }
+        }
+    }
+}
---
+++ DAN_LI_Bojana_Buljic/Zadatak_1/Validations/UsernameValidation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using Zadatak_1.Model;
+using Zadatak_1.Services;
+
+namespace Zadatak_1.Validations
+{
+    class UsernameValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string username = value as string;
+            Serivce ser= new Serivce();
+            List<tblUser> userList = ser.GetAllUsers();
+            var list = userList.Where(x => x.Username == username).ToList();            
+            if (list.Count() > 0)
+            {
+                return new ValidationResult(false, "This username already exists.");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
+    }
+}
