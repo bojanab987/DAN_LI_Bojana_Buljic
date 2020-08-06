6d64210cfe3cad6fbffb74499e0911ca4a4f3c47
bojanabb <bojana.buljic@gmail.com>
2020-08-06 13:36:29 +0200

    ddl script, EntityData Model add, Service class methods added

---
+++ DAN_LI_Bojana_Buljic/Zadatak_1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Zadatak_1.Commands;
+using Zadatak_1.View;
+
+namespace Zadatak_1.ViewModel
+{
+    class MainWindowViewModel:ViewModelBase
+    {
+        MainWindow main;
+
+        public MainWindowViewModel(MainWindow registrationView)
+        {
+            main = registrationView;
+        }
+
+       
+    }
+}
